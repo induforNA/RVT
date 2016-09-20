f19c4840676f9e1478d61bff61e0553add828054
ebycloudins <eby.sayone@gmail.com>
2016-09-20 18:53:10 +0530

    admin page

--- app/src/main/java/com/sayone/omidyar/view/AdminDashBoard.java
+++ app/src/main/java/com/sayone/omidyar/view/AdminDashBoard.java
@@ -1,12 +1,14 @@
 package com.sayone.omidyar.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.sayone.omidyar.R;
 
-public class AdminDashBoard extends AppCompatActivity {
+public class AdminDashBoard extends AppCompatActivity implements View.OnClickListener  {
     TextView rates,projectionTimeFrame,units,summaryOfSurveys;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,39 @@ public class AdminDashBoard extends AppCompatActivity {
         units=(TextView)findViewById(R.id.textview_units);
         summaryOfSurveys=(TextView)findViewById(R.id.textview_summary);
 
+        rates.setOnClickListener(this);
+        projectionTimeFrame.setOnClickListener(this);
+        units.setOnClickListener(this);
+        summaryOfSurveys.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()) {
+
+            case R.id.textview_rates:
+                intent=new Intent(getApplicationContext(),AdminRatesActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.textview_projection:
+                intent=new Intent(getApplicationContext(),ProjectionTimeFrameActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.textview_units:
+                intent=new Intent(getApplicationContext(),UnitsActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.textview_summary:
+                intent=new Intent(getApplicationContext(),SurveySummaryActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
 }
